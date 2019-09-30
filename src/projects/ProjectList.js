15d0f1509bb2376ba5b1169a2dcb19285726b58e
Craig McKeachie <craigdmckeachie@gmail.com>
2019-09-30 11:58:31 -0400

    lab08

--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Project } from './Project';
+import ProjectCard from './ProjectCard';
 
 class ProjectList extends React.Component {
   render() {
     const { projects } = this.props;
     const items = projects.map(project => (
       <div key={project.id} className="cols-sm">
-        <div className="card">
-          <img src={project.imageUrl} alt={project.name} />
-          <section className="section dark">
-            <h5 className="strong">
-              <strong>{project.name}</strong>
-            </h5>
-            <p>{project.description}</p>
-            <p>Budget : {project.budget.toLocaleString()}</p>
-          </section>
-        </div>
+        <ProjectCard project={project} />
       </div>
     ));
     return <div className="row">{items}</div>;
