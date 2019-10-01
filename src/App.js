a7b7832cf482207cecb773fb25a42219a41783be
Craig McKeachie <craigdmckeachie@gmail.com>
2019-10-01 12:25:54 -0400

    lab20

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import './App.css';
 import ProjectsPage from './projects/ProjectsPage';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from 'react-router-dom';
+import HomePage from './home/HomePage';
 
 function App() {
-  return <ProjectsPage />;
+  return (
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" exact className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects/" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/projects" exact component={ProjectsPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
